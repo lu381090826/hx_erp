32e726ac6741dcaab85252399fd80e68cf7f591d
lujagan <381090826@qq.com>
2017-07-07 21:06:07 +0800

    fix

--- application/views/login.php
+++ application/views/login.php
@@ -2,7 +2,7 @@
 <html>
 <head lang="en">
   <meta charset="UTF-8">
-  <title>Login Page | Amaze UI Example</title>
+  <title>登录页 | 韩迅系统</title>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="format-detection" content="telephone=no">
