d5ce3becceb010f7e4e2616c43d5ec3530e68c6a
lujagan <381090826@qq.com>
2017-07-07 21:04:53 +0800

    fix

--- application/views/head.php
+++ application/views/head.php
@@ -2,7 +2,7 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title>Landing Page | Amaze UI Example</title>
+    <title>韩迅系统</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport"
           content="width=device-width, initial-scale=1">
--- application/views/login.php
+++ application/views/login.php
@@ -30,7 +30,7 @@
 </div>
 <div class="am-g">
   <div class="am-u-lg-6 am-u-md-8 am-u-sm-centered">
-    <h3>韩讯系统登录</h3>
+    <h3>韩迅系统登录</h3>
     <hr>
     <br>
 
