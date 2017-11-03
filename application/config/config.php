4f49b48e2da9d962c371895823a562affcf443eb
gavinlu <gavinlu@lexinfintech.com>
2017-11-03 08:40:19 +0800

    feat:host

--- application/config/config.php
+++ application/config/config.php
@@ -234,7 +234,7 @@ $config['log_threshold'] = 1;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '../logs/';
+$config['log_path'] = '/data/log';
 
 /*
 |--------------------------------------------------------------------------
