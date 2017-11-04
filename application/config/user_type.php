8f5ade079a16f4401cbb345fcbe9383a62d7d356
gavinlu <gavinlu@lexinfintech.com>
2017-11-04 16:13:15 +0800

    feat:user_type

---
+++ application/config/user_type.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['user_type'] = array(
+    "admin" => 1,
+    "stock_admin" => 18,
+    "financial_admin" => 20,
+    "productor" => 21,
+    "seller" => 22,  //销售 role_id -22
+);
