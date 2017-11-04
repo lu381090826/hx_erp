c2c9e8dc1d4a5ef8cc920223172e2d2bdfcb77ff
gavinlu <gavinlu@lexinfintech.com>
2017-11-04 10:55:43 +0800

    feat:日志

--- application/helpers/utils_helper.php
+++ application/helpers/utils_helper.php
@@ -61,7 +61,7 @@ if (!function_exists('log_in')) {
         $in['uri'] = $CI->uri->rsegments;
         $in['benchmark'] = $CI->benchmark;
         $in['in_info'] = $request;
-        $in['user_session'] = $CI->session;
+        $in['user_session'] = isset($CI->session)?$CI->session:'';
         log_message('INFO',json_encode($in,JSON_UNESCAPED_UNICODE));
     }
 }
@@ -70,9 +70,9 @@ if (!function_exists('log_out')) {
     function log_out($request = '')
     {
         $CI = &get_instance();
-        $in['uri'] = $CI->uri->rsegments;
+        $in['benchmark'] = $CI->benchmark;
         $in['out_info'] = $request;
-        $in['user_session'] = $CI->session;
+        $in['user_session'] = isset($CI->session)?$CI->session:'';
         log_message('INFO',json_encode($in,JSON_UNESCAPED_UNICODE));
     }
 }
@@ -84,7 +84,7 @@ if (!function_exists('log_error')) {
         $in['uri'] = $CI->uri->rsegments;
         $in['benchmark'] = $CI->benchmark;
         $in['error_info'] = $request;
-        $in['user_session'] = $CI->session;
+        $in['user_session'] = isset($CI->session)?$CI->session:'';
         log_message('ERROR',json_encode($in,JSON_UNESCAPED_UNICODE));
     }
 }
