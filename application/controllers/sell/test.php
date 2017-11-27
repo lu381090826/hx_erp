2ec7449699e68a201cc71a0d8f046e58b2013423
EvonDu <298550274@qq.com>
2017-11-27 11:55:07 +0800

    添加商品模块需求接口

--- application/controllers/sell/test.php
+++ application/controllers/sell/test.php
@@ -14,8 +14,8 @@ class Test extends CI_Controller {
         $this->load->library('evon/ApiResult','','apiresult');
 
         //加载模型
-        $this->load->model('sell/form/FormSpu_model',"m_spu",true);
-        $this->load->model('sell/form/FormSku_model',"m_sku",true);
+        $this->load->model('sell/order/OrderSpu_model',"m_spu",true);
+        $this->load->model('sell/order/OrderSku_model',"m_sku",true);
     }
 
     /**
@@ -23,7 +23,34 @@ class Test extends CI_Controller {
      */
     public function index()
     {
-        $result = $this->m_sku->checkSkuExist("A124503052");
+        $this->getArrivalSpu();
+    }
+
+    /**
+     * 检测SPU/SKU是否有下单
+     */
+    public function checkExist(){
+        //查询SPU是否下单
+        $result = $this->m_spu->checkSpuExist("A1245");
         var_dump($result);
+        //查询SKU是否下单
+        $result = $this->m_sku->checkSkuExist("A12450305");
+        var_dump($result);
+    }
+
+    /**
+     * 根据SPU数组获取上新时间
+     */
+    public function getArrivalTime(){
+        $resut = $this->m_spu->getArrivalTime(array("A1245","test2017102901"));
+        var_dump($resut);
+    }
+
+    /**
+     * 根据SPU数组获取上新Spu
+     */
+    public function getArrivalSpu(){
+        $resut = $this->m_spu->getArrivalSpu("1511419480","1511419490");
+        var_dump($resut);
     }
 }
