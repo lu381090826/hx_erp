1cdc1f42654bf857d9e25707d27760626615c4b2
lujiaguan <381090826@qq.com>
2018-07-16 23:07:47 +0800

    脚本

--- script/pic.php
+++ script/pic.php
@@ -21,19 +21,46 @@ if ($link) {
     die("");
 }
 
-function query($link){
-    $sql = "SELECT top 1 * from pic.dbo.itempicture;";
+function query($link)
+{
+    //$sql = "select top ? * from pic.dbo.itempicture  where code not in ( select top ? code from pic.dbo.itempicture );";
+    $sql = "select top 100 * from pic.dbo.itempicture where code<'DAIXE8561' ORDER by code desc;";
+    $limit = 3;
+    $page = 0;
+    $params = array($limit,$page);
 
     $stmt = sqlsrv_query($link, $sql);
     if ($stmt === false) {
         die(print_r(sqlsrv_errors(), true));
     }
-    while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
 
         $code = $row["code"];
         $pic = $row["picture"];
+        savePic($code, $pic);
+    }
+}
 
-        print_r($code);
-        print_r($pic);
+
+function savePic($code = "", $jpg)
+{
+//生成图片
+    $imgDir = 'D:\\hanxun_pic\\';
+    $filename = $code . ".jpg";///要生成的图片名字
+    if (empty($jpg)) {
+        echo 'nostream';
+        exit();
+    }
+
+    $file = fopen($imgDir . $filename, "w");//打开文件准备写入
+    fwrite($file, $jpg);//写入
+    fclose($file);//关闭
+
+    $filePath = $imgDir . $filename;
+
+//图片是否存在
+    if (!file_exists($filePath)) {
+        echo 'createFail';
+        exit();
     }
 }
